bd6b130bca72cf7e4d65ef8e627955f264e65092
Olivier Riccini <olivier-webdev@hotmail.com>
2020-06-07 16:43:18 -0400

    change package json

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/blockchain-explorer'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/blockchain-explorer'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/blockchain-explorer/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/blockchain-explorer/index.html'));
+});
 
+// default Heroku PORT
 app.listen(process.env.PORT || 9999);
